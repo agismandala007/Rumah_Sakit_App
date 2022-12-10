48e7c9d22195ca8c4d9050b941c5772d91ff865a
agismandala007 <agixsatria@gmail.com>
2022-12-10 20:25:00 +0700

    add beberapa layoput

---
+++ app/src/main/java/com/example/tugas_proyek/Bayar.java
@@ -0,0 +1,14 @@
+package com.example.tugas_proyek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bayar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bayar);
+    }
+}
--- app/src/main/java/com/example/tugas_proyek/Konsul.java
+++ app/src/main/java/com/example/tugas_proyek/Konsul.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Check_in extends AppCompatActivity {
+public class Konsul extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_check_in);
+        setContentView(R.layout.activity_konsul);
     }
 }
